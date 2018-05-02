26d5f8b68ff5e54477bf485d2d8a6a8486149764
LAPTOP-EG55A78C\ananda <anandadwi20@gmail.com>
2018-05-02 15:07:46 +0700

    config base url heroku version 11

--- application/views/setup/index.php
+++ application/views/setup/index.php
@@ -15,15 +15,15 @@
   <?php $this->load->view($content); ?>
 </body>
 
-<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.11.1/jquery.validate.min.js'></script>
+<script src="<?php echo base_url(); ?>assets/login/js/jquery.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/login/js/jquery.validate.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/login/js/index.js"></script>
 <script>
 	$(document).ready(function () {
 	    $("#loginform").submit(function () {
 
 	        $.ajax({
-	            url: "<?php echo site_url('http://milki.herokuapp.com/login/ceklogin');?>",
+	            url: "<?php echo site_url('login/ceklogin');?>",
 	            type: "POST",
 	            data: {
 	                username: $('#Username').val(),
