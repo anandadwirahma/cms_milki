82160f38140e5a51e2624c21376215020cab5bcc
LAPTOP-EG55A78C\ananda <anandadwi20@gmail.com>
2018-05-02 12:46:46 +0700

    config base url heroku version

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,8 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/cms_milki/';
+// $config['base_url'] = 'http://localhost/cms_milki/';
+$config['base_url'] = 'https://milki.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
