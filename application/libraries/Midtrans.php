50b09e8ca058a3bcddd23dad41f8c52279f2730b
LAPTOP-EG55A78C\ananda <anandadwi20@gmail.com>
2018-05-10 23:15:17 +0700

    bug fixing 15.0

--- application/libraries/Midtrans.php
+++ application/libraries/Midtrans.php
@@ -132,7 +132,7 @@ class Midtrans {
 	      $result_array = json_decode($result);
 	      if ($info['http_code'] != 201 && !in_array($result_array->status_code, array(200, 201, 202, 407)) ) {
         $message = 'Midtrans Error (' . $info['http_code'] . '): '; 
-          . implode(',', (array) $result_array->error_message);
+          . implode(',', (array) $result_array->error_messages);
         throw new Exception($message, $info['http_code']);
 
       }
